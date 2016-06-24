07efc32beccc78468d2ba300e7874604e868cf70
Tony Brix <Tony@Brix.ninja>
2016-06-24 00:06:34 -0500

    wrap callback in process.nextTick

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
-"use strict";
-
-const promisificator = (callback) => {
-	let promise;
-	if (typeof callback === "undefined") {
-		promise = new Promise((resolve, reject) => {
-			callback = function(err, value) {
-				if (err) {
-					reject(err);
-				} else if (arguments.length <= 2) {
-					resolve(value);
-				} else {
-					let values = Array.from(arguments).slice(1);
-					resolve(values);
-				}
+const promisificator = (cb) => {
+	let promise, callback;
+	switch (typeof cb) {
+		case "undefined":
+			promise = new Promise((resolve, reject) => {
+				callback = function (err, value) {
+					if (err) {
+						reject(err);
+					} else if (arguments.length <= 2) {
+						resolve(value);
+					} else {
+						let values = Array.from(arguments).slice(1);
+						resolve(values);
+					}
+				};
+			});
+			break;
+		case "function":
+			callback = function () {
+				process.nextTick(cb, ...arguments);
 			};
-		});
+			break;
+		default:
+			throw new Error("First argument must be a function or undefined.");
 	}
 	return {
 		promise,
