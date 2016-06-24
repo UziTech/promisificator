fdbcfbec3ab980e953c5d63fa9ace41e6a8de3b8
Tony Brix <Tony@Brix.ninja>
2016-06-23 20:23:23 -0500

    add test

---
+++ spec/index.js
@@ -0,0 +1,12 @@
+const promisificator = require("../src/index.js");
+
+const {
+    promise,
+    callback,
+} = promisificator();
+
+promise
+	.then(console.log)
+	.catch(console.error);
+
+callback(null, "test");
