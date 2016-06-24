b846551fc0396dad249514eec49c6988531fa48a
Tony Brix <Tony@Brix.ninja>
2016-06-23 19:50:21 -0500

    update index.js

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const promisificator = (callback) => {
-	return (resolve, reject) => {
-		callback = (err, value) => {
-			if (err) {
-				reject(err);
-			} else if (arguments.length === 2) {
-				resolve(value);
-			} else {
-				let values = Array.from(arguments).slice(1);
-				resolve(values);
-			}
-		};
+	let promise;
+	if (typeof callback === "undefined") {
+		promise = new Promise((resolve, reject) => {
+			callback = (err, value) => {
+				if (err) {
+					reject(err);
+				} else if (arguments.length === 2) {
+					resolve(value);
+				} else {
+					let values = Array.from(arguments).slice(1);
+					resolve(values);
+				}
+			};
+		});
+	}
+	return {
+		promise,
+		callback,
 	};
 };
 
